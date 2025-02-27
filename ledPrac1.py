8f3d55ad2f3d2635d4be16ed4791166337e55192
aaryan-siingh <146904867+aaryan-siingh@users.noreply.github.com>
2025-02-27 20:52:15 +0530

    Create ledPrac1.py
    IOT Practical 2

---
+++ ledPrac1.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.output(ledPin, False)
+try:
+    while True:
+        GPIO.output(ledPin, True)
+        print("LED ON")
+        sleep(1) 
+        GPIO.output(ledPin, False) 
+        print("LED OFF")
+        sleep(1) 
+
+finally:
+    GPIO.output(ledPin, False)
+    GPIO.cleanup()
